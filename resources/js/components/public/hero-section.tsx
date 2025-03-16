704692994393ea4afbd64e3ba601692111672892
Jun <55792006+Junveloper@users.noreply.github.com>
2025-03-16 23:53:12 +1000

    Update hero-section.tsx

--- resources/js/components/public/hero-section.tsx
+++ resources/js/components/public/hero-section.tsx
@@ -3,10 +3,10 @@ import { Separator } from '../ui/separator';
 
 export default function HeroSection() {
     return (
-        <div className="h-[400px] bg-cover bg-center" style={{ backgroundImage: `url(${heroImage})` }}>
-            <div className="flex h-full w-full flex-col bg-zinc-950/70">
-                <div className="my-auto flex w-full flex-col items-center space-y-10 py-6 backdrop-blur-sm">
-                    <div className="font-arcade text-center text-5xl font-bold tracking-wide text-white uppercase">
+        <div className="h-[500px] bg-cover bg-center" style={{ backgroundImage: `url(${heroImage})` }}>
+            <div className="bg-background/70 flex h-full w-full flex-col">
+                <div className="my-auto flex w-full flex-col items-center space-y-10 py-6 backdrop-blur-[4px]">
+                    <div className="font-arcade text-foreground text-center text-5xl font-bold tracking-wide uppercase">
                         Descend into the Gaming Dungeon
                     </div>
 
@@ -14,7 +14,7 @@ export default function HeroSection() {
                         <Separator className="w-30" />
                     </div>
 
-                    <div className="max-w-md space-y-4 text-center text-base font-light text-white">
+                    <div className="text-foreground max-w-md space-y-4 text-center text-base font-light">
                         <p>A modern-day arcade where 67 gaming PCs await you</p>
                         <p>Right in the heart of Brisbane City</p>
                     </div>
