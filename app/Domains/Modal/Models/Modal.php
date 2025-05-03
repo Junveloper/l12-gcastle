32e2a3033908a089d47c3a18dc175414fa03c1c4
Jun <55792006+Junveloper@users.noreply.github.com>
2025-05-03 18:31:33 +1000

    modal model

---
+++ app/Domains/Modal/Models/Modal.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domains\Modal\Models;
+
+use App\Domains\App\Traits\Model\HasUuid;
+use Carbon\CarbonImmutable;
+use Database\Factories\ModalFactory;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property int $id
+ * @property string $uuid
+ * @property string $display_from
+ * @property string $display_to
+ * @property string $title
+ * @property string|null $title_display_colour
+ * @property string $content
+ * @property CarbonImmutable|null $created_at
+ * @property CarbonImmutable|null $updated_at
+ *
+ * @method static ModalFactory factory($count = null, $state = [])
+ * @method static Builder<static>|Modal newModelQuery()
+ * @method static Builder<static>|Modal newQuery()
+ * @method static Builder<static>|Modal query()
+ * @method static Builder<static>|Modal whereContent($value)
+ * @method static Builder<static>|Modal whereCreatedAt($value)
+ * @method static Builder<static>|Modal whereDisplayFrom($value)
+ * @method static Builder<static>|Modal whereDisplayTo($value)
+ * @method static Builder<static>|Modal whereId($value)
+ * @method static Builder<static>|Modal whereTitle($value)
+ * @method static Builder<static>|Modal whereTitleDisplayColour($value)
+ * @method static Builder<static>|Modal whereUpdatedAt($value)
+ * @method static Builder<static>|Modal whereUuid($value)
+ *
+ * @mixin \Eloquent
+ */
+class Modal extends Model
+{
+    use HasFactory;
+    use HasUuid;
+}
--- database/migrations/2025_05_03_081738_create_modals_table.php
+++ database/migrations/2025_05_03_081738_create_modals_table.php
@@ -11,7 +11,6 @@ return new class extends Migration
         Schema::create('modals', function (Blueprint $table) {
             $table->id();
             $table->uuid('uuid')->unique();
-            $table->string('status');
             $table->dateTime('display_from');
             $table->dateTime('display_to');
             $table->string('title');
