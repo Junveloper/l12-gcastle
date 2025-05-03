a4085f248699bb7625bb592e7c62c73029db4b63
Jun <55792006+Junveloper@users.noreply.github.com>
2025-05-03 20:32:31 +1000

    add modal seeder

---
+++ database/seeders/ModalSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Domains\Modal\Models\Modal;
+use Illuminate\Database\Seeder;
+
+class ModalSeeder extends Seeder
+{
+    public function run()
+    {
+        Modal::factory()->create([
+            'title' => 'We are open 24/7',
+            'content' => '<p>We are open on Labour Day (5th May)</p>',
+            'title_display_colour' => '#30de8c',
+            'display_from' => now(),
+            'display_to' => now()->addWeeks(2),
+        ]);
+    }
+}
